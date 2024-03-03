b24c2ffd63f9c6ea1b83f4420e72a5bf8033da75
evergreenies <evergreenlief@gmail.com>
2024-03-03 10:03:03 +0530

    Added two sum problem

---
+++ python_algorithms/arrays/017_two_sum.py
@@ -0,0 +1,36 @@
+"""
+**Challenge:**
+You are given an array of **unique integers** and a **specific target sum**. 
+Your task is to find the **indices** of the **two** elements within the array 
+that **add up to the target sum**. It's important to note that:
+
+* Each input will have **exactly one valid solution**.
+* You cannot use the same element **twice**.
+
+**Example:**
+* **Input:** `[2, 7, 11, 15]`, `target = 9`
+* **Output:** `(0, 1)` (Explanation: `nums[0] + nums[1] = 2 + 7 = 9`)
+
+**Clarifications:**
+* The input array contains **unique** integers.
+* The output is a tuple containing the **indices** of the two elements, not 
+the elements themselves.
+"""
+
+
+def two_sum(arr: list[int], target: int) -> tuple[int, int] | tuple:
+    if not arr:
+        return ()
+
+    visited = {}
+    for index, num in enumerate(arr):
+        complement = target - num
+        if complement in visited:
+            return (visited[complement], index)
+        visited[num] = index
+
+    return ()
+
+
+if __name__ == "__main__":
+    assert two_sum([2, 7, 11, 15], 9) == (0, 1)
