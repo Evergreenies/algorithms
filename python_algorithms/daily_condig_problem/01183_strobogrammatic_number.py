9b668c7864c55a3957311a572d7fae043ebbb737
evergreenies <evergreenlief@gmail.com>
2024-03-05 22:43:41 +0530

    added strobogrammatic

--- python_algorithms/daily_condig_problem/01183_strobogrammatic_number.py
+++ python_algorithms/daily_condig_problem/01183_strobogrammatic_number.py
@@ -5,6 +5,9 @@ being rotated 180 degrees. For example, 16891 is strobogrammatic.
 Create a program that finds all strobogrammatic numbers with N digits.
 """
 
+from typing import Iterator
+
+
 STROBO_MAP = {"0": "0", "1": "1", "8": "8", "6": "9", "9": "6"}
 
 
@@ -23,6 +26,42 @@ def is_strobogrammatic_number(num: str) -> bool:
         return True
 
 
+def find_strobogrammatic_in_range(digit: int) -> Iterator:
+    if digit == 0:
+        return []
+
+    start = 10 ** (digit - 1)
+    end = (10**digit) - 1
+
+    while start <= end:
+        if is_strobogrammatic_number(str(start)):
+            yield start
+        start += 1
+
+
+def collect_strabogrammatics(digit: int) -> list:
+    refined_stobogrammatic = []
+    for number in find_strobogrammatic_in_range(digit):
+        refined_stobogrammatic.append(number)
+
+    return refined_stobogrammatic
+
+
 if __name__ == "__main__":
     assert is_strobogrammatic_number("16891") is True
     assert is_strobogrammatic_number("1689") is False
+    assert collect_strabogrammatics(3) == [
+        101,
+        111,
+        181,
+        609,
+        619,
+        689,
+        808,
+        818,
+        888,
+        906,
+        916,
+        986,
+    ]
+    assert collect_strabogrammatics(2) == [11, 69, 88, 96]
