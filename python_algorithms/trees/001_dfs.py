40d76bfb0e16b71d547bcc2a2071c3d65ca3c9e4
evergreenies <evergreenlief@gmail.com>
2024-03-07 13:27:08 +0530

    updated DCP01177

--- python_algorithms/trees/001_dfs.py
+++ python_algorithms/trees/001_dfs.py
@@ -27,7 +27,20 @@ def dfs(root: Node | None) -> list[Any]:
     return result
 
 
+def dfs_recursive(root: Node | None) -> list[Any]:
+    if root is None:
+        return []
+
+    left = dfs_recursive(root.left)
+    right = dfs_recursive(root.right)
+
+    return [root.value] + left + right
+
+
 if __name__ == "__main__":
     _root = Node("a", Node("b", Node("d"), Node("e")), Node("c", None, Node("f")))
     assert dfs(_root) == ["a", "b", "d", "e", "c", "f"]
     assert dfs(None) == []
+
+    assert dfs_recursive(_root) == ["a", "b", "d", "e", "c", "f"]
+    assert dfs_recursive(None) == []
