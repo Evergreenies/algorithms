100b8e4fa06a43482fd03c18614437475c9eb085
evergreenies <evergreenlief@gmail.com>
2024-03-07 16:18:44 +0530

    find target in tree by BFS

--- python_algorithms/trees/003_find_target_in_tree.py
+++ python_algorithms/trees/003_find_target_in_tree.py
@@ -28,9 +28,34 @@ def find_target_in_tree_by_dfs(root: Node, target: Any) -> bool:
     return False
 
 
+def find_target_in_tree_by_bfs(root: Node, target: Any) -> bool:
+    if root is Node:
+        return False
+
+    queue = [root]
+    while len(queue) > 0:
+        current = queue.pop(0)
+
+        if current.value == target:
+            return True
+
+        if current.left:
+            queue.append(current.left)
+
+        if current.right:
+            queue.append(current.right)
+
+    return False
+
+
 if __name__ == "__main__":
     _root = Node("a", Node("b", Node("d"), Node("e")), Node("c", None, Node("f")))
     assert find_target_in_tree_by_dfs(_root, "a") is True
     assert find_target_in_tree_by_dfs(_root, "e") is True
     assert find_target_in_tree_by_dfs(_root, "f") is True
     assert find_target_in_tree_by_dfs(_root, "z") is False
+
+    assert find_target_in_tree_by_bfs(_root, "a") is True
+    assert find_target_in_tree_by_bfs(_root, "e") is True
+    assert find_target_in_tree_by_bfs(_root, "f") is True
+    assert find_target_in_tree_by_bfs(_root, "z") is False
