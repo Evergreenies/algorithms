6572a5092eb3a579847f89b6e8718816f8d52086
evergreenies <evergreenlief@gmail.com>
2024-10-24 09:19:29 +0530

    Added DCP01393 Python

---
+++ python_algorithms/daily_coding_problem/01393_josephus_problem.py
@@ -0,0 +1,20 @@
+"""
+There are N prisoners standing in a circle, waiting to be executed. The executions are carried out starting with the kth person, and removing every successive kth person going clockwise until there is no one left.
+
+Given N and k, write an algorithm to determine where a prisoner should stand in order to be the last survivor.
+
+For example, if N = 5 and k = 2, the order of executions would be [2, 4, 1, 5, 3], so you should return 3.
+
+Bonus: Find an O(log N) solution if k = 2.
+"""
+
+
+def josephus_problem(n: int) -> int:
+    _l = 1
+    while _l * 2 <= n:
+        _l *= 2
+
+    return 2 * (n - _l) + 1
+
+
+print(josephus_problem(5))
